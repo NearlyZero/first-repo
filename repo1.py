cd996379299b1af12796d01f57ab80227b66119b
Andrew <54544958+NearlyZero@users.noreply.github.com>
2020-02-15 12:08:37 -0500

    newcode

---
+++ repo1.py
@@ -0,0 +1,6 @@
+names = ["Jim,", "Karena", "Kimmi", "Maryann", "Pete", "Kieran", "Jaya Gem", 
+"James B", "Sam", "Caitlin"]
+print()
+for index, name in enumerate(names, start=1):
+    print(index, name)
+print()
