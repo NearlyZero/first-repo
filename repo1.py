4b985d19ea2d4d6a480742f7597b19a25dfc4e75
Andrew <54544958+NearlyZero@users.noreply.github.com>
2020-11-21 11:19:31 -0500

    Delete repo1.py

--- repo1.py
+++
@@ -1,6 +0,0 @@
-names = ["Jim,", "Karena", "Kimmi", "Maryann", "Pete", "Kieran", "Jaya Gem", 
-"James B", "Sam", "Caitlin"]
-print()
-for index, name in enumerate(names, start=1):
-    print(index, name)
-print()
